58b43a510f3ca83b8983c242f929702801673393
SalmanSayeed79 <salmansayeed5345@gmail.com>
2023-09-01 01:13:07 +0600

    Plugins Completed

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
 import { useEffect } from 'react';
-import { BrowserRouter ,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Social from './Pages/Social';
 import Marketplace from './Pages/Marketplace';
 import Create from './Pages/Create';
 import Profile from './Pages/Profile';
+import Plugins from './Pages/Plugins';
+import PluginContextProvider from './Hooks/PluginContextProvider';
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/marketplace" element={<Marketplace/>}/>
-        <Route path="/social" element={<Social/>}/>
-        <Route path="/create" element={<Create/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-      </Routes>
+      <PluginContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="/plugins" element={<Plugins />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </PluginContextProvider>
     </BrowserRouter>
   );
 }
---
+++ src/Data/Plugins.jsx
@@ -0,0 +1,57 @@
+export const pluginsData=[
+    {
+        id:"map",
+        name:"Survey Bot Plugin",
+        title:"A plugin that lets you see the data collected by different robots on a mapview", 
+        image:"https://i.postimg.cc/N03G43rH/Green-Watercolor-Leaf-on-Light-Yellow-Background-Square-Pillow-3.png"
+    },
+    {
+        id:"drone",
+        name:"Drone View Plugin",
+        title:"A plugin that lets you view the live feed of your drones",
+        image:"https://i.postimg.cc/KYkvtG4b/Green-Watercolor-Leaf-on-Light-Yellow-Background-Square-Pillow.png"
+    },
+    {
+        id:"remote",
+        name:"Rover Control Plugin",
+        title:"A plugin that lets you control your rover bots from distance",
+        image:"https://i.postimg.cc/bJ1yFmQF/Green-Watercolor-Leaf-on-Light-Yellow-Background-Square-Pillow-2.png"
+    },
+    {
+        id:"rfid",
+        name:"Entry Management Plugin",
+        title:"A plugin that lets you manage who enters what room when",
+        image:"https://i.postimg.cc/Dy6ZTpR9/26761063-2106-i201-007-F-m004-c9-call-center-technical-support-isometric.jpg"
+    }
+]
+export const pluginItems={
+    "map":{
+        id:"map",
+        name:"Survey Bot",
+        title:"A plugin that lets you see the data collected by different robots on a mapview", 
+        image:"https://i.postimg.cc/N03G43rH/Green-Watercolor-Leaf-on-Light-Yellow-Background-Square-Pillow-3.png",
+        path:"/mapplugin"
+
+    },
+    "drone":{
+        id:"drone",
+        name:"Drone View",
+        title:"A plugin that lets you view the live feed of your drones",
+        image:"https://i.postimg.cc/KYkvtG4b/Green-Watercolor-Leaf-on-Light-Yellow-Background-Square-Pillow.png",
+        path:"/droneplugin"
+    },
+    "remote":{
+        id:"remote",
+        name:"Rover Control",
+        title:"A plugin that lets you control your rover bots from distance",
+        image:"https://i.postimg.cc/bJ1yFmQF/Green-Watercolor-Leaf-on-Light-Yellow-Background-Square-Pillow-2.png",
+        path:"/remoteplugin"
+    },
+    "rfid":{
+        id:"rfid",
+        name:"Entry Management Plugin",
+        title:"A plugin that lets you manage who enters what room when",
+        image:"https://i.postimg.cc/Dy6ZTpR9/26761063-2106-i201-007-F-m004-c9-call-center-technical-support-isometric.jpg",
+        path:"/rfidplugin"
+    }
+}
