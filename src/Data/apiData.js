0f7bcfab805e6e02c00a3ea7e3b5e1533abd6b9c
SalmanSayeed79 <salmansayeed5345@gmail.com>
2023-09-21 02:41:46 +0600

    login and create account implemented

--- src/Data/apiData.js
+++ src/Data/apiData.js
@@ -1,3 +1,7 @@
+export  const API_ENDPOINT="http://3.26.32.106:8081"
 export const API_ENDPOINT_FRONT="http://localhost:3000"
 export const PARAPHRASER_API="http://localhost:1234"
 export const IMAGE_GENERATOR_API="http://127.0.0.1:7860"
+
+//chmod 0400 {PEMKey}
+//ssh -i {PEMKey} ec2-user@3.26.32.106
